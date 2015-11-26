cce4b186aa375fdd0345befbc4c380a3aeacad9a
BelqesAhmed <ahmed.belqes@gmail.com>
2015-11-26 16:40:37 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <?php
+echo("hgjhgjhgjhgjhg");
+?>
+        <a href="222.php">222</a>
+    </body>
+</html>
